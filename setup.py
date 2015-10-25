096cda8c66531c3322f0acea1a415c256a54d95e
Paris <paris@sourcelair.com>
2015-10-25 16:56:25 +0200

    Bump version to 0.2 and add LICENSE file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='Jason',
-      version='0.1',
+      version='0.2',
       description='Thin Django-like object mapper for REST services',
       author='Paris Kasidiaris',
       author_email='pariskasidiaris@gmail.com',
